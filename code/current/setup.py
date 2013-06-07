8060a00bb616c64b4b38294e90c84ed09d971b58
Ross Fadely <rossfadely@gmail.com>
2013-06-07 10:57:22 -0300

    remove testing module

--- code/current/setup.py
+++ code/current/setup.py
@@ -18,11 +18,5 @@ extmod2 = Extension('_fit_models',
                     sources = [src_dir+'_fit_models.c'])
 extmod3 = Extension('_coeff_marginalization',
                     sources = [src_dir+'_coeff_marginalization.c'])
-extmod4 = Extension('foo',
-                    sources = [src_dir+'foo.c',
-                               src_dir+'get_num_files.c',
-                               src_dir+'calc_normalization.c',
-                               src_dir+'read_file.c',
-                               src_dir+'get_filelength.c'])
 
-setup(ext_modules=[extmod1,extmod2,extmod3,extmod4])
+setup(ext_modules=[extmod1,extmod2,extmod3])
