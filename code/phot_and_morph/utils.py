fa9d567c795d0bcc0fdebdbd61b06f433567349d
rossfadely <rossfadely@gmail.com>
2014-09-23 13:34:49 +0000

    adding utils

--- code/phot_and_morph/utils.py
+++ code/phot_and_morph/utils.py
@@ -7,6 +7,169 @@ import os
 import numpy as np
 import pyfits as pf
 
+def log_multivariate_gaussian(x, mu, V, Vinv=None, method=1):
+    """
+    Swiped from astroML:
+    https://github.com/astroML/astroML/blob/master/astroML/utils.py
+
+    Evaluate a multivariate gaussian N(x|mu, V)
+
+    This allows for multiple evaluations at once, using array broadcasting
+
+    Parameters
+    ----------
+    x: array_like
+        points, shape[-1] = n_features
+
+    mu: array_like
+        centers, shape[-1] = n_features
+
+    V: array_like
+        covariances, shape[-2:] = (n_features, n_features)
+
+    Vinv: array_like or None
+        pre-computed inverses of V: should have the same shape as V
+
+    method: integer, optional
+        method = 0: use cholesky decompositions of V
+        method = 1: use explicit inverse of V
+
+    Returns
+    -------
+    values: ndarray
+        shape = broadcast(x.shape[:-1], mu.shape[:-1], V.shape[:-2])
+
+    Examples
+    --------
+
+    >>> x = [1, 2]
+    >>> mu = [0, 0]
+    >>> V = [[2, 1], [1, 2]]
+    >>> log_multivariate_gaussian(x, mu, V)
+    -3.3871832107434003
+    """
+    x = np.asarray(x, dtype=float)
+    mu = np.asarray(mu, dtype=float)
+    V = np.asarray(V, dtype=float)
+
+    ndim = x.shape[-1]
+    x_mu = x - mu
+
+    if V.shape[-2:] != (ndim, ndim):
+        raise ValueError("Shape of (x-mu) and V do not match")
+
+    Vshape = V.shape
+    V = V.reshape([-1, ndim, ndim])
+
+    if Vinv is not None:
+        assert Vinv.shape == Vshape
+        method = 1
+
+    if method == 0:
+        Vchol = np.array([linalg.cholesky(V[i], lower=True)
+                          for i in range(V.shape[0])])
+
+        # we may be more efficient by using scipy.linalg.solve_triangular
+        # with each cholesky decomposition
+        VcholI = np.array([linalg.inv(Vchol[i])
+                          for i in range(V.shape[0])])
+        logdet = np.array([2 * np.sum(np.log(np.diagonal(Vchol[i])))
+                           for i in range(V.shape[0])])
+
+        VcholI = VcholI.reshape(Vshape)
+        logdet = logdet.reshape(Vshape[:-2])
+
+        VcIx = np.sum(VcholI * x_mu.reshape(x_mu.shape[:-1]
+                                            + (1,) + x_mu.shape[-1:]), -1)
+        xVIx = np.sum(VcIx ** 2, -1)
+
+    elif method == 1:
+        if Vinv is None:
+            Vinv = np.array([linalg.inv(V[i])
+                             for i in range(V.shape[0])]).reshape(Vshape)
+        else:
+            assert Vinv.shape == Vshape
+
+        logdet = np.log(np.array([linalg.det(V[i])
+                                  for i in range(V.shape[0])]))
+        logdet = logdet.reshape(Vshape[:-2])
+
+        xVI = np.sum(x_mu.reshape(x_mu.shape + (1,)) * Vinv, -2)
+        xVIx = np.sum(xVI * x_mu, -1)
+
+    else:
+        raise ValueError("unrecognized method %s" % method)
+
+    return -0.5 * ndim * np.log(2 * np.pi) - 0.5 * (logdet + xVIx)
+
+def logsumexp(arr, axis=None):
+    """
+    Swiped from astroML:
+    https://github.com/astroML/astroML/blob/master/astroML/utils.py
+    
+    Computes the sum of arr assuming arr is in the log domain.
+
+    Returns log(sum(exp(arr))) while minimizing the possibility of
+    over/underflow.
+
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> a = np.arange(10)
+    >>> np.log(np.sum(np.exp(a)))
+    9.4586297444267107
+    >>> logsumexp(a)
+    9.4586297444267107
+    """
+    # if axis is specified, roll axis to 0 so that broadcasting works below
+    if axis is not None:
+        arr = np.rollaxis(arr, axis)
+        axis = 0
+
+    # Use the max to normalize, as with the log this is what accumulates
+    # the fewest errors
+    vmax = arr.max(axis=axis)
+    out = np.log(np.sum(np.exp(arr - vmax), axis=axis))
+    out += vmax
+
+    return out
+
+def fetch_mixed_epoch(epoch, gal_frac=0.5, shuffle=True, seed=12345):
+    """
+    Make a combined sample of stars and galaxies for a given epoch.  Return single epoch and 
+    coadd data.
+    """
+    # fetch
+    ss, sc = fetch_epoch(epoch, 'stars')
+    gs, gc = fetch_epoch(epoch, 'gals')
+
+    # size
+    N = np.minimum(ss.field(0).size, gs.field(0).size)
+    Ngal = np.round(gal_frac * N)
+    Nstar = N - Ngal
+
+    # shuffle:
+    if shuffle:
+        np.random.seed(seed)
+        ind = np.random.permutation(N).astype(np.int)
+    else:
+        ind = np.arange(N, dtype=np.int)
+
+    # cut
+    ss, sc = ss[:Nstar], sc[:Nstar]
+    gs, gc = gs[:Ngal], gc[:Ngal]
+
+    # build
+    os = {}
+    oc = {}
+    for k in ss._coldefs.names:
+        os[k] = np.append(ss.field(k), gs.field(k))[ind]
+        if k != 'coadd_objid':
+            oc[k] = np.append(sc.field(k), gc.field(k))[ind]
+
+    return os, oc
+
 def fetch_epoch(epoch, kind, verbose=False):
     """
     Return the single epoch and the matched coadded data.
@@ -64,7 +227,7 @@ def fetch_epoch(epoch, kind, verbose=False):
         tbhdu.writeto(ddir + 's82coadd_%s_%d.fits' % (kind, epoch), clobber=True)
         c = tbhdu.data
     
-    return c
+    return s, c
 
 if __name__ == '__main__':
     import time
